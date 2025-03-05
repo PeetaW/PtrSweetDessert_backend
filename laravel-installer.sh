8ebe3ef0afff38afceb1a1c10c6212c110df3804
PeetaW <h7337473@hotmail.com.tw>
2025-03-05 17:28:54 +0800

    Buildup laravel 11 & Laravel sail & docker-based environment

---
+++ laravel-installer.sh
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="refresh" content="0;url='https://laravel.com/docs'" />
+
+        <title>Redirecting to https://laravel.com/docs</title>
+    </head>
+    <body>
+        Redirecting to <a href="https://laravel.com/docs">https://laravel.com/docs</a>.
+    </body>
+</html>
