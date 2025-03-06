41d127b390546c2f7821fdd6877adb7000463b90
PeetaW <h7337473@hotmail.com.tw>
2025-03-07 00:48:10 +0800

    添加了api.php，並將路由網址挪至api.php裡定義了
    不過由於先前就已手動安裝laravel sanctum，所以此時額外安裝api.php似乎意義不大，但已完成login與register API設計，接下來轉往前端開始準備測試API溝通

--- PtrSweetDessert_backend/routes/web.php
+++ PtrSweetDessert_backend/routes/web.php
@@ -1,14 +1,9 @@
 <?php
 
-use App\Http\Controllers\AuthController;
-use Illuminate\Http\Request;
+
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return response()->json($request->user());
-});
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
+
