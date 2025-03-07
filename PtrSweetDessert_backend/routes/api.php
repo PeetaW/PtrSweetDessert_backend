84655a357748237eafee1d8f763849439db4cfa8
PeetaW <h7337473@hotmail.com.tw>
2025-03-07 16:59:22 +0800

    修改中介層group function

--- PtrSweetDessert_backend/routes/api.php
+++ PtrSweetDessert_backend/routes/api.php
@@ -4,9 +4,12 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return response()->json($request->user());
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('/user', function (Request $request) {
+        return response()->json($request->user());
+    });
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
+
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
