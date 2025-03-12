98d10578b17f79dfde45dbafdcd483d35fc3afe7
PeetaW <h7337473@hotmail.com.tw>
2025-03-12 18:45:23 +0800

    嘗試解決CORS & CSRF等問題中

--- PtrSweetDessert_backend/config/cors.php
+++ PtrSweetDessert_backend/config/cors.php
@@ -3,7 +3,7 @@
 return [
   'paths' => ['api/*', 'sanctum/csrf-cookie'],
   'allowed_methods' => ['*'],
-  'allowed_origins' => ['*'], // 或指定特定前端網址，如 ['http://localhost:5173']
+  'allowed_origins' => ['http://localhost:8080/'], // 或指定特定前端網址，如 ['http://localhost:5173']
   'allowed_origins_patterns' => [],
   'allowed_headers' => ['*'],
   'exposed_headers' => [],
